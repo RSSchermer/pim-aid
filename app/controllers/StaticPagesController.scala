98499b28ff608561327c873f2eba56b4fc016f90
R.S. Schermer <roland0507@gmail.com>
2015-03-12 15:06:25 +0100

    Accidentally broke tests when trying to optimize usersession side-loading

--- app/controllers/StaticPagesController.scala
+++ app/controllers/StaticPagesController.scala
@@ -8,17 +8,5 @@ object StaticPagesController extends Controller {
   def introduction = Action {
     Ok(html.staticPages.introduction())
   }
-
-  def privacy = Action {
-    Ok(html.staticPages.privacy())
-  }
-
-  def about = Action {
-    Ok(html.staticPages.about())
-  }
-
-  def contact = Action {
-    Ok(html.staticPages.contact())
-  }
 }
 
